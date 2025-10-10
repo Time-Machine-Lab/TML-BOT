896965f85b9e4bff4bb92ec5a40915c00a8c6ab8
geniusay <969025903@qq.com>
2025-10-10 21:47:43 +0800

    feat(modelmaster): 添加ModelMaster服务配置和AI视频生成功能
    新增ModelMaster服务的JSON配置文件和核心处理模块
    实现AI视频生成命令处理逻辑，支持从用户输入获取提示词
    将原helloworld命令重构为专门的genAIVideo处理函数

---
+++ core/handler/modelmaster_handle.py
@@ -0,0 +1,29 @@
+from astrbot import logger
+from astrbot.api.event import filter
+from astrbot.api.star import Context, Star, StarTools, register
+from astrbot.core import AstrBotConfig
+
+class ModelMasterHandler:
+
+    def __init__(self, context: Context ,config: AstrBotConfig):
+        self.context = context
+        self.conf = config
+        self.api_url = self.conf["mm_api_url"]
+        self.api_key = self.conf["mm_api_key"]
+        self.headers = {
+            "secret": f"{self.api_key}",
+            "Content-Type": "application/json",
+        }
+
+    async def genAIVideo(self, event: AiocqhttpMessageEvent):
+        """生成AI视频"""
+        raw = event.message_str.removeprefix("生成AI视频").strip()
+        user_name = event.get_sender_name()
+        if not raw:
+            yield event.plain_result("未检测到AI视频生成提示词，请补充")
+            return
+       
+        logger.info(f"{user_name} 准备生成AI视频：{raw}")
+
+        yield event.plain_result(f"ModelMaster已生成视频, 提示词{raw}.") # 发送一条纯文本消息
+        
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from .core.handler import ModelMasterHandler
+from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
+    AiocqhttpMessageEvent,
+)
 
 @register("TML-Bot", "TML", "Just For TML", "1.0.0")
 class TMLBotPlugin(Star):
@@ -8,21 +12,14 @@ class TMLBotPlugin(Star):
         super().__init__(context)
 
     async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("[mm]AI视频", alias={"AI视频"})
-    async def helloworld(self, event: AstrMessageEvent):
-        """生产AI视频"""
-        raw = event.message_str.removeprefix("[mm]AI视频").removeprefix("AI视频").strip()
-        user_name = event.get_sender_name()
-        if not raw:
-            yield event.plain_result("未检测到AI视频生成提示词，请补充")
-            return
-       
-        logger.info(f"{user_name} 准备生成AI视频：{raw}")
+        self.modelMaster = ModelMasterHandler(self.context, self.conf)
 
-        yield event.plain_result(f"ModelMaster已生成视频, 提示词{raw}.") # 发送一条纯文本消息
+    
+    # ModelMaster
+    @filter.command("生成AI视频", desc="生成AI视频 xxxx")
+    async def genAIVideo(self, event: AiocqhttpMessageEvent):
+        await self.modelMaster.genAIVideo(event)
+
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
